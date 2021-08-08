1884d8680ede3f90911afeb712323756671b2fd2
srinevas48 <72512665+srinevas48@users.noreply.github.com>
2021-08-08 16:29:20 +0530

    Update ProvincePojo.java

--- ProvincePojo.java
+++ ProvincePojo.java
@@ -1,152 +1,48 @@
-package com.codechallenge.core.pojos;
+package com.aig.dcp.nextgen.core.pojo;
+
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class ProvincePojo {
 
-	@SerializedName("ID")
+	@SerializedName("id")
 	@Expose
-	private String iD;
-	@SerializedName("Country")
+	private String id;
+	@SerializedName("title")
 	@Expose
-	private String country;
-	@SerializedName("CountryCode")
+	private String title;
+	
+	@SerializedName("price")
 	@Expose
-	private String countryCode;
-	@SerializedName("Province")
+	private String price;
+	@SerializedName("description")
 	@Expose
-	private String province;
-	@SerializedName("City")
+	private String description;
+	@SerializedName("category")
 	@Expose
-	private String city;
-	@SerializedName("CityCode")
+	private String category;
+	@SerializedName("image")
 	@Expose
-	private String cityCode;
-	@SerializedName("Lat")
-	@Expose
-	private String lat;
-	@SerializedName("Lon")
-	@Expose
-	private String lon;
-	@SerializedName("Confirmed")
-	@Expose
-	private Integer confirmed;
-	@SerializedName("Deaths")
-	@Expose
-	private Integer deaths;
-	@SerializedName("Recovered")
-	@Expose
-	private Integer recovered;
-	@SerializedName("Active")
-	@Expose
-	private Integer active;
-	@SerializedName("Date")
-	@Expose
-	private String date;
-
-	public String getID() {
-		return iD;
+	private String image;
+	
+	public String getid() {
+		return id;
 	}
-
-	public void setID(String iD) {
-		this.iD = iD;
+	public String getTitle() {
+		return title;
 	}
-
-	public String getCountry() {
-		return country;
+	public String getPrice() {
+		return price;
 	}
-
-	public void setCountry(String country) {
-		this.country = country;
+	public String getDescription() {
+		return description;
 	}
-
-	public String getCountryCode() {
-		return countryCode;
+	public String getCategory() {
+		return category;
 	}
-
-	public void setCountryCode(String countryCode) {
-		this.countryCode = countryCode;
+	public String getImage() {
+		return image;
 	}
-
-	public String getProvince() {
-		return province;
-	}
-
-	public void setProvince(String province) {
-		this.province = province;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getCityCode() {
-		return cityCode;
-	}
-
-	public void setCityCode(String cityCode) {
-		this.cityCode = cityCode;
-	}
-
-	public String getLat() {
-		return lat;
-	}
-
-	public void setLat(String lat) {
-		this.lat = lat;
-	}
-
-	public String getLon() {
-		return lon;
-	}
-
-	public void setLon(String lon) {
-		this.lon = lon;
-	}
-
-	public Integer getConfirmed() {
-		return confirmed;
-	}
-
-	public void setConfirmed(Integer confirmed) {
-		this.confirmed = confirmed;
-	}
-
-	public Integer getDeaths() {
-		return deaths;
-	}
-
-	public void setDeaths(Integer deaths) {
-		this.deaths = deaths;
-	}
-
-	public Integer getRecovered() {
-		return recovered;
-	}
-
-	public void setRecovered(Integer recovered) {
-		this.recovered = recovered;
-	}
-
-	public Integer getActive() {
-		return active;
-	}
-
-	public void setActive(Integer active) {
-		this.active = active;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
-
+	
 }
